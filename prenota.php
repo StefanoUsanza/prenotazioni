b3bbd4e1851dcbc14cecd0195c4f384d2800b302
StefanoUsanza <stefano.usanza@gmail.com>
2021-03-13 09:41:50 +0100

    refactor db configuration using an external file

--- prenota.php
+++ prenota.php
@@ -1,19 +1,6 @@
 <?php
 
-//mostra i messaggi di errori ma non vengono loggati
-ini_set('display_errors', 1);
-ini_set('log_errors', 0);
-
-
-$host = 'localHost';
-$db = 'app_prenotazioni';
-$user = 'root';
-$pass = '';
-$charset = 'utf8';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
-$pdo= new PDO($dsn, $user, $pass);
+include_once "config.php";
 
 $codice_fiscale= $_POST['codice'];
 $giorno = $_POST['giorno'];
@@ -31,3 +18,6 @@ $stm-> execute(
     'giorno' => $giorno
 ]
 );
+
+header('location:lista_prenotazioni.php');
+exit(0);
